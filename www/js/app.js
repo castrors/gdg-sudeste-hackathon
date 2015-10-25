1a91806e1bd168c0dd13e7aacd1faef1e7160ffa
Arthur Braga Alfredo <arthurbalfredo@gmail.com>
2015-10-25 12:41:17 -0200

    Places para os pontos turísticos adicionados

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var myApp = angular.module('starter', ['ionic']);
+var lat = -19.936066;
+var lon = -43.936592;
 
 myApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -19,26 +21,81 @@ myApp.run(function($ionicPlatform) {
 });
 
 myApp.controller('MapController', function($scope, $ionicLoading) {
- 
-    google.maps.event.addDomListener(window, 'load', function() {
-        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
- 
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
- 
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                title: "My Location"
-            });
-        });
-        $scope.map = map;
+
+  var myLatlng;
+
+
+  google.maps.event.addDomListener(window, 'load', function() {
+    //myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        map: map,
+        title: "My Location"
+      });
+      lat = pos.coords.latitude;
+      lon = pos.coords.longitude;
     });
+    $scope.map = map;
+
+    initMap();
+  });
+
+
+  function initMap() {
+
+    //var pyrmont = {lat: -33.867, lng: 151.195};
+
+    myLatlng = new google.maps.LatLng(lat,lon);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatlng,
+      zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: myLatlng,
+      radius: 1500,
+      types: ["library", "art_gallery", "bus_station", "movie_theater", "museum","church", "park", "shopping_mall", "stadium"]
+
+    }, callback);
+  }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+
+
+
+
 });
